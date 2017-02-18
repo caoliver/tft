484bcd151b8d33aa8fe21f8ec5c10a4cc04282b6
Christopher A. Oliver <caowasteland@gmail.com>
2017-02-17 19:01:47 -0500

    Add incremental restriction

--- editor.lua
+++ editor.lua
@@ -1,3 +1,21 @@
+--[[
+function opendebug(ptsnum)
+   if debugout then debugout:close() end
+   debugout=io.open('/dev/pts/'..tostring(ptsnum), 'w')
+   if not debugout then print('debug closed'); return; end
+   print('debug set to /dev/pts/'..tostring(ptsnum))
+end
+
+function debug(...)
+   if not debugout then return end
+   for i,val in ipairs {...} do
+      if i > 1 then debugout:write '\t' end
+      debugout:write(tostring(val))
+   end
+   debugout:write '\r\n'
+end
+--]]
+
 local l = require 'ljcurses'
 local a = l.attributes
 local b = l.boxes
@@ -22,8 +40,8 @@ function edit_tagset(tagset)
    for k in pairs(tagset.categories) do table.insert(series_sorted, k) end
    table.sort(series_sorted)
    local current_series
-   local category
-   local packages_window
+   local package_list
+   local package_window
    if not tagset.package_cursors then tagset.package_cursors = {} end
    local package_cursors = tagset.package_cursors
    local package_cursor = package_cursors[current_series] or 1
@@ -40,67 +58,72 @@ function edit_tagset(tagset)
    local tagformat = '%-'..maxtaglen..'s'
    local state
    local descr_window
-   local anchor
-   local searchpat=''
-   local search_nomatch
+   local has_restriction
+   local all_series
 
-   local function show_series()
-      l.move(1, 9)
-      l.clrtoeol()
-      l.move(1, cols-1)
-      l.addch(b.vline)
-      l.move(1, 9)
-      local name = series_sorted[current_series]
-      local descrs = tagset.category_description[name]
-      l.addnstr(name.. (descrs and (' - '..descrs.short) or ''), cols - 10)
-      l.noutrefresh()
+   local function make_restricted_list (restriction, series_number)
+      local result = {}
+      local series_set =
+	 series_number and { series_number } or series_sorted
+      if pcall(string.find, '', restriction) then
+	 for _, series in ipairs(series_set) do
+	    local cat = tagset.categories[series]
+	    for _, tuple in ipairs(cat) do
+	       if tuple.tag:find(restriction) then
+		  table.insert(result, tuple)
+	       end
+	    end
+	 end
+      end
+      return result
+   end
+
+   local function show_restriction()
+      if has_restriction then
+	 l.move(rows-2, cols - #has_restriction-2)
+	 local color
+	 if #package_list == 0 then
+	    color = colors.nomatch
+	 elseif all_series then
+	    color = colors.all
+	 else
+	    color = colors.highlight
+	 end
+	 l.attron(color)
+	 l.addstr(has_restriction)
+	 l.attroff(color)
+	 l.attron(colors.main)
+      end
    end
 
    local function draw_package(tuple, line, selected)
-      l.move(packages_window, line, 0)
-      l.clrtoeol(packages_window)
+      l.move(package_window, line, 0)
+      l.clrtoeol(package_window)
       local outstr = tagformat:format(tuple.tag)
       if tuple.shortdescr then
 	 outstr = outstr..' - '..tuple.shortdescr
       end
-      l.attron(packages_window, colors[tuple.state])
-      l.addstr(packages_window, state_signs[tuple.state])
-      l.attroff(packages_window, colors[tuple.state])
+      l.attron(package_window, colors[tuple.state])
+      l.addstr(package_window, state_signs[tuple.state])
+      l.attroff(package_window, colors[tuple.state])
       if tuple.state == tuple.old_state then
-	 l.addstr(packages_window, ' ')
+	 l.addstr(package_window, ' ')
       else
-	 l.attron(packages_window, colors[tuple.old_state])
-	 l.addch(packages_window, b.diamond)
-	 l.attroff(packages_window, colors[tuple.old_state])
+	 l.attron(package_window, colors[tuple.old_state])
+	 l.addch(package_window, b.diamond)
+	 l.attroff(package_window, colors[tuple.old_state])
       end
       if selected then
-	 l.attron(packages_window, colors.highlight)
+	 l.attron(package_window, colors.highlight)
 	 local outmax=cols-4
-	 if state ~= 'search' or search_nomatch then
-	    l.addnstr(packages_window, outstr, outmax)
-	 else
-	    local first, rest
-	    if anchor then
-	       first, rest = '',outstr:sub(#searchpat+1)
-	    else
-	       local start, finish = outstr:find(searchpat, 1, true)
-	       first, rest = outstr:sub(1,start-1),outstr:sub(finish+1,-1)
-	    end
-	    l.addnstr(packages_window, first, outmax)
-	    local outmax = outmax - #first
-	    if outmax > 0 then
-	       l.attron(packages_window, a.standout)
-	       l.addnstr(packages_window, searchpat, outmax)
-	       l.attroff(packages_window, a.standout)
-	       outmax = outmax - #searchpat
-	       if outmax > 0 then
-		  l.addnstr(packages_window, rest, outmax)
-	       end
-	    end
-	 end
-	 l.attroff(packages_window, colors.highlight)
+	 l.addnstr(package_window, outstr, outmax)
+	 l.attroff(package_window, colors.highlight)
+	 l.move(1, 9)
+	 l.clrtoeol()
+	 l.move(1, cols-1)
+	 l.addch(b.vline)
 	 l.move(1, cols-10)
-	 l.addnstr(''..package_cursor..'/'..#category, 9)
+	 l.addnstr(''..package_cursor..'/'..#package_list, 9)
 	 l.move(rows-2,2)
 	 l.clrtoeol()
 	 l.move(rows-2, cols-1)
@@ -119,36 +142,32 @@ function edit_tagset(tagset)
 	    end
 	    l.addnstr(pkgdescr, cols - 4)
 	 end
-	 if #searchpat > 0 then
-	    l.move(rows-2, cols - #searchpat - 2)
-	    local standout =
-	       search_nomatch and colors.nomatch or colors.pattern
-	    l.attron(standout)
-	    l.addstr(searchpat)
-	    l.attroff(standout)
-	    l.attron(colors.main)
-	 end
+	 l.move(1, 9)
+	 local descrs=tagset.category_description[tuple.category]
+	 l.addnstr(tuple.category ..
+		      (descrs and (' - '..descrs.short) or ''), cols - 10)
       else
-	 l.addnstr(packages_window, outstr, cols-4)
+	 l.addnstr(package_window, outstr, cols-4)
       end
    end
 
    local function redraw_package_list()
-      if not packages_window then
-	 packages_window = l.newwin(subwin_lines,cols-2,3,1)
-	 l.bkgd(packages_window, l.color_pair(1))
+      if not package_window then
+	 package_window = l.newwin(subwin_lines,cols-2,3,1)
+	 l.bkgd(package_window, l.color_pair(1))
       end
       local cursor = package_cursor
       viewport_top = package_cursor - subwin_lines / 2
       if viewport_top < 1 then viewport_top = 1 end
       local top = viewport_top
       for i=1,subwin_lines do
-	 local tuple=category[top+i-1]
+	 local tuple=package_list[top+i-1]
 	 if not tuple then break end
 	 draw_package(tuple, i-1, package_cursor == top+i-1)
 	 cursor = cursor+1
       end
-      l.noutrefresh(packages_window)
+      show_restriction()
+      l.noutrefresh(package_window)
    end
 
    local function draw_description()
@@ -178,15 +197,15 @@ function edit_tagset(tagset)
       l.addch(b.rtee)
       l.move(1,1)
       l.addstr('Series:')
-      show_series()
       l.move(rows-3,0)
       l.addch(b.ltee)
       l.hline(b.hline, cols-2)
       l.move(rows-3,cols-1)
       l.addch(b.rtee)
-      if packages_window then
-	 l.delwin(packages_window)
-	 packages_window = nil
+      l.noutrefresh()
+      if package_window then
+	 l.delwin(package_window)
+	 package_window = nil
       end
       redraw_package_list()
       if descr_window then
@@ -199,13 +218,41 @@ function edit_tagset(tagset)
       -- What else do we need to redraw here?
    end
 
+   local function select_restrict(restriction, all_series)
+      if not restriction then
+	 if has_restriction then
+	    has_restriction = nil
+	    package_list = tagset.categories[series_sorted[current_series]]
+	    repaint()
+	 end
+	 return
+      end
+
+      package_list = {}
+      local series_set =
+	 all_series and series_sorted or { series_sorted[current_series] }
+      if pcall(string.find, '', restriction) then
+	 for _, series in ipairs(series_set) do
+	    local cat = tagset.categories[series]
+	    for _, tuple in ipairs(cat) do
+	       if tuple.tag:find(restriction) then
+		  table.insert(package_list, tuple)
+	       end
+	    end
+	 end
+      end
+      has_restriction = restriction
+      repaint()
+   end
+
    local function select_series(new_series)
+      if has_restriction then select_restrict(false) end
       if new_series ~= current_series then
 	 if current_series then
 	    package_cursors[current_series] = package_cursor
 	 end
 	 package_cursor = package_cursors[new_series] or 1
-	 category = tagset.categories[series_sorted[new_series]]
+	 package_list = tagset.categories[series_sorted[new_series]]
 	 current_series = new_series
 	 repaint()
       end
@@ -219,7 +266,7 @@ function edit_tagset(tagset)
 	 tuple.state = new_state
 	 tagset.dirty = true
 	 draw_package(tuple, line, true)
-	 repaint()
+	 l.noutrefresh(package_window)
       end
    end
 
@@ -252,36 +299,36 @@ function edit_tagset(tagset)
 	    show_descr = nil
 	    repaint()
 	    goto continue
-	 elseif state == 'search' then
-	    if key == 27 then
-	       state = nil
-	       searchpat=''
-	       repaint()
-	       goto continue
-	    elseif char == '^' then
-	       anchor = not anchor
-	    elseif delete_keys[key] then
-	       if #searchpat > 0 then
-		  searchpat = searchpat:sub(1,-2)
-	       end
-	    elseif key == k.down then
-	    elseif key == k.up then
-	    elseif key == k.home then
-	    elseif char and search_char[char] and #searchpat < searchmax then
-	       searchpat = searchpat..char
-	    end
-	    redraw_package_list()
-	    -- incremental char
 	 end
 	 if key == k.ctrl_l then
 	    repaint()
-	 elseif char == '/' then
-	    state = 'search'
 	 -- Show description
-	 elseif char == 'd' then
+	 elseif key == k.ctrl_d then
 	    state = 'describe'
-	    show_descr = category[package_cursor].description
+	    show_descr = package_list[package_cursor].description
 	    repaint()
+	 -- Restriction
+	 elseif key == k.escape then
+	    select_restrict()
+	 elseif key == k.unitsep then
+	    if (has_restriction) then
+	       all_series = not all_series
+	       select_restrict(has_restriction, all_series)
+	    end
+	 elseif key >= 32 and key <= 126 then
+	    if not has_restriction then
+	       has_restriction=''
+	       all_series = nil
+	    end
+	    if #has_restriction < 16 then
+	       has_restriction = has_restriction..char
+	       select_restrict(has_restriction, all_series)
+	    end
+	 elseif delete_keys[key] then
+	    if has_restriction and #has_restriction > 0 then
+	       has_restriction = has_restriction:sub(1, -2)
+	       select_restrict(has_restriction, all_series)
+	    end
 	 -- Navigation
 	 elseif key == k.right then
 	    if current_series < #series_sorted then
@@ -291,23 +338,28 @@ function edit_tagset(tagset)
 	    if current_series > 1 then
 	       select_series(current_series-1)
 	    end
+	 elseif char == '<' then
+	    select_series(1)
+	 elseif char == '>' then
+	    select_series(#series_sorted)
 	 elseif key == k.home then
 	    package_cursor = 1
 	    repaint()
 	 elseif key == k['end'] then
-	    package_cursor = #category
+	    package_cursor = #package_list
 	    repaint()
 	 elseif key == k.down then
-	    if package_cursor < #category then
+	    if package_cursor < #package_list then
 	       package_cursor = package_cursor+1
 	       if package_cursor == viewport_top + subwin_lines then
 		  repaint()
 	       else
-		  draw_package(category[package_cursor-1],
+		  draw_package(package_list[package_cursor-1],
 			       package_cursor-viewport_top-1, false)
-		  draw_package(category[package_cursor],
+		  draw_package(package_list[package_cursor],
 			       package_cursor-viewport_top, true)
-		  l.noutrefresh(packages_window)
+		  show_restriction()
+		  l.noutrefresh(package_window)
 	       end
 	    end
 	 elseif key == k.up then
@@ -316,18 +368,19 @@ function edit_tagset(tagset)
 	       if package_cursor < viewport_top then
 		  repaint()
 	       else
-		  draw_package(category[package_cursor+1],
+		  draw_package(package_list[package_cursor+1],
 			       package_cursor-viewport_top+1, false)
-		  draw_package(category[package_cursor],
+		  draw_package(package_list[package_cursor],
 			       package_cursor-viewport_top, true)
-		  l.noutrefresh(packages_window)
+		  show_restriction()
+		  l.noutrefresh(package_window)
 	       end
 	    end
 	 elseif key == k.page_down then
-	    if package_cursor < #category then
+	    if package_cursor < #package_list then
 	       package_cursor = package_cursor + subwin_lines / 2
-	       if package_cursor > #category then
-		  package_cursor = #category
+	       if package_cursor > #package_list then
+		  package_cursor = #package_list
 	       end
 	       repaint()
 	    end
@@ -339,28 +392,25 @@ function edit_tagset(tagset)
 	       end
 	       repaint()
 	    end
-	 elseif char == '<' then
-	    select_series(1)
-	 elseif char == '>' then
-	    select_series(#series_sorted)
-	 elseif char == '+' then
-	    change_state(category[package_cursor], 'ADD',
+	 -- Change package state
+	 elseif key == k.ctrl_a then
+	    change_state(package_list[package_cursor], 'ADD',
 			 package_cursor-viewport_top)
-	 elseif char == '-' then
-	    change_state(category[package_cursor], 'SKP',
+	 elseif key == k.ctrl_s then
+	    change_state(package_list[package_cursor], 'SKP',
 			 package_cursor-viewport_top)
-	 elseif char == 'o' then
-	    change_state(category[package_cursor], 'OPT',
+	 elseif key == k.ctrl_o then
+	    change_state(package_list[package_cursor], 'OPT',
 			 package_cursor-viewport_top)
-	 elseif char == 'r' then
-	    change_state(category[package_cursor], 'REC',
+	 elseif key == k.ctrl_r  then
+	    change_state(package_list[package_cursor], 'REC',
 			 package_cursor-viewport_top)
-	 elseif char == '=' then
-	    change_state(category[package_cursor],
-			 category[package_cursor].old_state,
+	 elseif key == k.ctrl_u  then
+	    change_state(package_list[package_cursor],
+			 package_list[package_cursor].old_state,
 			 package_cursor-viewport_top)
 	 elseif char == ' ' then
-	    change_state(category[package_cursor], nil,
+	    change_state(package_list[package_cursor], nil,
 			 package_cursor-viewport_top)
 	 end
       end
@@ -381,13 +431,15 @@ function edit_tagset(tagset)
    colors.SKP = bit.bor(l.color_pair(4), a.bold)
    colors.OPT = bit.bor(l.color_pair(5), a.bold)
    colors.REC = 0
-   colors.pattern = colors.OPT
+   colors.pattern = colors.highlight
    colors.nomatch = colors.SKP
+   colors.all = colors.OPT
    colors.main= bit.bor(l.color_pair(1), a.bold)
    l.bkgd(colors.main)
    l.attron(colors.main)
    l.refresh()
    local result={pcall(do_editor)}
+   select_restrict(false)
    l.endwin()
    if not result[1] then print(unpack(result)) end
    tagset.current_series = current_series
