9cb935342f9e2adc8c95d8b3ad0a361a82d1fc1f
Christopher A. Oliver <caowasteland@gmail.com>
2017-03-05 07:14:01 -0500

    Add usleep()

--- util.c
+++ util.c
@@ -36,6 +36,12 @@ LUAFN(cputime)
     return 1;
 }
 
+LUAFN(usleep)
+{
+    usleep(luaL_checkinteger(L, 1));
+    return 0;
+}
+
 LUAFN(xxhsum_file)
 {
     int fd;
@@ -66,6 +72,7 @@ LUALIB_API int luaopen_util(lua_State *L)
     static const luaL_Reg funcptrs[] = {
 //	FN_ENTRY(catch_signals),
 	FN_ENTRY(cputime),
+	FN_ENTRY(usleep),
 	FN_ENTRY(xxhsum_file),
 	FN_ENTRY(lib_exists),
 	{ NULL, NULL }
